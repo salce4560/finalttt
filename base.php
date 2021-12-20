566a7d2fdbf3cb594e147be5f246e1d8e91cc6bf
mackliu <macklun@gmail.com>
2021-12-20 09:43:09 +0800

    在base.php中增加後台各功能會使用到的字串轉換功能

--- base.php
+++ base.php
@@ -6,12 +6,69 @@ class DB{
     protected $dsn="mysql:host=localhost;charset=utf8;dbname=web01";
     protected $user="root";
     protected $pw='';
-    protected $table;
     protected $pdo;
+    public $table;
+    public $title;
+    public $button;
+    public $header;
+    public $append;
 
     public function __construct($table){
         $this->table=$table;
         $this->pdo=new PDO($this->dsn,$this->user,$this->pw);
+        $this->setStr($table);
+    }
+
+    private function setStr($table){
+        switch($table){
+            case "title";
+                $this->title="網站標題管理";
+                $this->button="新增網站標題圖片";
+                $this->header="網站標題";
+            break;
+            case "ad";
+            $this->title="動態文字廣告管理";
+            $this->button="新增動態文字廣告";
+            $this->header="動態文字廣告";
+            break;
+            case "mvim";
+            $this->title="動畫圖片管理";
+            $this->button="新增動畫圖片";
+            $this->header="動畫圖片";
+            break;
+            case "image";
+            $this->title="校園映像資料管理";
+            $this->button="新增校園映像圖片";
+            $this->header="校園映像資料圖片";
+            break;
+            case "total";
+            $this->title="進站總人數管理";
+            $this->button="";
+            $this->header="進站總人數:";
+            break;
+            case "bottom";
+            $this->title="頁尾版權資料管理";
+            $this->button="";
+            $this->header="頁尾版權資料";
+            break;
+            case "news";
+            $this->title="最新消息資料管理";
+            $this->button="新增最新消息資料";
+            $this->header="最新消息資料內容";
+            break;
+            case "admin";
+            $this->title="管理者帳號管理";
+            $this->button="新增管理者帳號";
+            $this->header="帳號";
+            $this->append="密碼";
+            break;
+            case "menu";
+            $this->title="選單管理";
+            $this->button="新增主選單";
+            $this->header="主選單名稱";
+            $this->append="選單連結網址";
+            break;
+        }
     }
 
     public function find($id){
@@ -139,6 +196,40 @@ $Image=new DB('image');
 $News=new DB('news');
 $Admin=new DB('admin');
 $Menu=new DB('menu');
+
+//$tt=(isset($_GET['do']))?$_GET['do']:'';
+//$tt=isset($_GET['do'])??'';
+$tt=$_GET['do']??'';
+
+switch($tt){
+    case "title":
+        $DB=$Title;
+    break;
+    case "ad":
+        $DB=$Ad;
+    break;
+    case "mvim":
+        $DB=$Mvim;
+    break;
+    case "image":
+        $DB=$Image;
+    break;
+    case "total":
+        $DB=$Total;
+    break;
+    case "bottom":
+        $DB=$Bottom;
+    break;
+    case "news":
+        $DB=$News;
+    break;
+    case "admin":
+        $DB=$Admin;
+    break;
+    case "menu":
+        $DB=$Menu;
+    break;
+}
 /* $total=$Total->find(1);
 
 //echo $Total->find(1)['total'];
