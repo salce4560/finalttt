bb43c264eab3fb316aa292602d630f0667594a59
mackliu <macklun@gmail.com>
2021-12-24 11:36:43 +0800

    完成動畫圖片輪播功能

--- front/main.php
+++ front/main.php
@@ -3,14 +3,30 @@
     <?php include "marquee.php";?>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
+
+    <div style="width:100%; padding:2px; height:290px;">
+        <div id="mwww" loop="true" style="width:100%; height:100%;">
+            <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
+        </div>
+    </div>
     <script>
     var lin = new Array();
+    <?php
+        //撈出所有要顯示的動畫圖片並放入JS陣列中
+        $mvs=$Mvim->all(['sh'=>1]);
+        foreach($mvs as $mv){
+        ?>
+            lin.push('<?="img/{$mv['img']}";?>')
+        <?php    
+        }
+
+    ?>
     var now = 0;
+    ww()   //先執行一次
     if (lin.length > 1) {
         setInterval("ww()", 3000);
         now = 1;
     }
-
     function ww() {
         $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
         //$("#mwww").attr("src",lin[now])
@@ -19,11 +35,6 @@
             now = 0;
     }
     </script>
-    <div style="width:100%; padding:2px; height:290px;">
-        <div id="mwww" loop="true" style="width:100%; height:100%;">
-            <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
-        </div>
-    </div>
     <div
         style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
